1a24998544d95a4d76b7f20bcb52fd7496766a4a
Alfred Warui <alfredwarui17@gmail.com>
2021-07-23 17:50:59 +0300

    add initial lines of code

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $(".design").click(function(){
+    $("design-showing").toggle();
+    $("design-hidden").toggle();
+  });
+
+  $(".design").click(function(){
+    $("").toggle();
+    $("").toggle();
+  });
+
+  $(".design").click(function(){
+    $("").toggle();
+    $("").toggle();
+  });
+
+});
