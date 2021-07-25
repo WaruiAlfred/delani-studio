ab4f20b07644a791ebd819fd63bd4f428fee4f5d
Alfred Warui <alfredwarui17@gmail.com>
2021-07-25 13:46:28 +0300

    add mouse function to create hover effect over porfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  //what we do
   $(".design").click(function(){
     $(".design-showing").toggle();
     $(".design-hidden").toggle();
@@ -15,4 +16,56 @@ $(document).ready(function(){
     $(".product-management-hidden").toggle();
   });
 
+   //portfolio
+  $("#work4").mouseover(function(){
+    $("#overtext4").show();
+  }).mouseout(function(){
+    $("#overtext4").hide();
+  });
+
+  $("#work3").mouseover(function(){
+    $("#overtext3").show();
+  }).mouseout(function(){
+    $("#overtext3").hide();
+  });
+
+  $("#work2").mouseover(function(){
+    $("#overtext2").show();
+  }).mouseout(function(){
+    $("#overtext2").hide();
+  });
+
+  $("#work1").mouseover(function(){
+    $("#overtext1").show();
+  }).mouseout(function(){
+    $("#overtext1").hide();
+  });
+
+  $("#work5").mouseover(function(){
+    $("#overtext5").show();
+  }).mouseout(function(){
+    $("#overtext5").hide();
+  });
+
+  $("#work6").mouseover(function(){
+    $("#overtext6").show();
+  }).mouseout(function(){
+    $("#overtext6").hide();
+  });
+
+  $("#work7").mouseover(function(){
+    $("#overtext7").show();
+  }).mouseout(function(){
+    $("#overtext7").hide();
+  });
+
+  $("#work8").mouseover(function(){
+    $("#overtext8").show();
+  }).mouseout(function(){
+    $("#overtext8").hide();
+  });
+
 });
+ 
+
+   
